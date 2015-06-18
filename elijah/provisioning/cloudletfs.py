6fe655d18170fed3b0347a717555a8a34c0a7a43
Kiryong Ha <krha@cmu.edu>
2015-06-18 16:54:00 -0400

    code cleaning of cloudletfs.py

--- elijah/provisioning/cloudletfs.py
+++ elijah/provisioning/cloudletfs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 #
 # Cloudlet Infrastructure for Mobile Computing
 #
@@ -26,48 +26,51 @@ import threading
 import multiprocessing
 import time
 import sys
-import log as logging
-import Queue
-from Configuration import Const
+from . import log as logging
+from .Configuration import Const
 
 LOG = logging.getLogger(__name__)
 
-
-# system.py is built at install time, so pylint may fail to import it.
-# Also avoid warning on variable name.
-# pylint: disable=F0401,C0103
-# pylint: enable=F0401,C0103
 
 class CloudletFSError(Exception):
     pass
 
 
 class CloudletFS(threading.Thread):
-    FUSE_TYPE_DISK      =   "disk"
-    FUSE_TYPE_MEMORY    =   "memory"
+    FUSE_TYPE_DISK = "disk"
+    FUSE_TYPE_MEMORY = "memory"
 
-    def __init__(self, bin_path, args, modified_disk_chunks=None, modified_memory_chunks=None, **kwargs):
+    def __init__(
+            self,
+            bin_path,
+            args,
+            modified_disk_chunks=None,
+            modified_memory_chunks=None,
+            **kwargs):
         self.cloudletfs_path = bin_path
-        self._args = '%d\n%s\n' % (len(args),
-                '\n'.join(a.replace('\n', '') for a in args))
+        self._args = '%d\n%s\n' % (
+            len(args),
+            '\n'.join(a.replace('\n', '') for a in args))
         self._pipe = None
         self.mountpoint = None
         self.stop = threading.Event()
-        self.modified_disk_chunks = list(modified_disk_chunks) if modified_disk_chunks else None
-        self.modified_memory_chunks = list(modified_memory_chunks) if modified_memory_chunks else None
+        self.modified_disk_chunks = list(
+            modified_disk_chunks) if modified_disk_chunks else None
+        self.modified_memory_chunks = list(
+            modified_memory_chunks) if modified_memory_chunks else None
 
         # fuse can handle on-demand fetching
-        # TODO: passing these argument through kwargs
+        # TODO: passing these arguments through kwargs
         self.demanding_queue = kwargs.get("demanding_queue", None)
         self.meta_info = kwargs.get("meta_info", None)
         threading.Thread.__init__(self, target=self.fuse_read)
 
     def fuse_read(self):
         wait_statistics = list()
-        if (self.meta_info != None) and (self.demanding_queue != None):
+        if (self.meta_info is not None) and (self.demanding_queue is not None):
             memory_overlay_dict = dict()
             disk_overlay_dict = dict()
-            from Configuration import Const
+            from .Configuration import Const
             for blob in self.meta_info[Const.META_OVERLAY_FILES]:
                 overlay_url = blob[Const.META_OVERLAY_FILE_NAME]
                 memory_chunks = blob[Const.META_OVERLAY_FILE_MEMORY_CHUNKS]
@@ -79,11 +82,11 @@ class CloudletFS(threading.Thread):
 
         while(not self.stop.wait(0.001)):
             self._running = True
-            select.select([self.proc.stdout],[],[])
+            select.select([self.proc.stdout], [], [])
             oneline = self.proc.stdout.readline()
             if len(oneline.strip()) > 0:
                 request_split = oneline.split(",")
-                if (self.demanding_queue != None) and \
+                if (self.demanding_queue is not None) and \
                         (len(request_split) > 0) and \
                         (request_split[0].find("REQUEST") > 0):
                     overlay_type = request_split[1].split(":")[1].strip()
@@ -96,8 +99,8 @@ class CloudletFS(threading.Thread):
                         msg = "FUSE type does not match : %s" % overlay_type
                         raise CloudletFSError(msg)
 
-                    if url == None:
-                        msg = "Cannot find matching blob with chunk(%ld)" % chunk
+                    if url is None:
+                        msg = "Can't find matching blob with chunk(%ld)" % chunk
                         raise CloudletFSError(msg)
                     #LOG.debug("requesting chunk(%ld) at %s" % (chunk, url))
                     self.demanding_queue.put(url)
@@ -105,7 +108,10 @@ class CloudletFS(threading.Thread):
                     type_name, overlay_type = request_split[1].split(":")
                     chunk_name, chunk = request_split[2].split(":")
                     wait_name, wait_time = request_split[3].split(":")
-                    data = {type_name:overlay_type, chunk_name:long(chunk.strip()), wait_name:float(wait_time.strip())}
+                    data = {
+                        type_name: overlay_type, chunk_name: long(
+                            chunk.strip()), wait_name: float(
+                            wait_time.strip())}
                     wait_statistics.append(data)
             else:
                 LOG.info("FUSE pipe is closed")
@@ -115,8 +121,11 @@ class CloudletFS(threading.Thread):
             total_wait_time = 0.0
             for item in wait_statistics:
                 total_wait_time += item['time']
-            LOG.info("%d chunks waited for synthesizing for avg %f s, total: %f s" % \
-                    (len(wait_statistics), total_wait_time/len(wait_statistics), total_wait_time))
+            LOG.info(
+                "%d chunks waited for synthesizing for avg %f s, total: %f s" %
+                (len(wait_statistics),
+                 total_wait_time / len(wait_statistics),
+                 total_wait_time))
         else:
             LOG.info("NO chunks has been waited at FUSE")
         self._running = False
@@ -131,13 +140,13 @@ class CloudletFS(threading.Thread):
     def launch(self):
         read, write = os.pipe()
         try:
-            self.proc = subprocess.Popen([self.cloudletfs_path], stdin=read,
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                    close_fds=True)
+            self.proc = subprocess.Popen(
+                [self.cloudletfs_path], stdin=read, stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE, close_fds=True)
             self._pipe = os.fdopen(write, 'w')
             self._pipe.write(self._args)
             self._pipe.flush()
-            out = self.proc.stdout.readline() 
+            out = self.proc.stdout.readline()
             self.mountpoint = out.strip()
         except:
             if self._pipe is not None:
@@ -147,11 +156,11 @@ class CloudletFS(threading.Thread):
             raise
         finally:
             pass
-            #os.close(read)
-        if os.path.exists(self.mountpoint) == False:
+            # os.close(read)
+        if os.path.exists(self.mountpoint) is False:
             msg = "Failed to mount FUSE file system\n"
             msg += "  1. Check FUSE permission of /dev/fuse to have '666'\n"
-            msg += "  2. Check FUSE configuration at /etc/fuse.conf to have 'allow_others' option\n"
+            msg += "  2. Change FUSE conf (/etc/fuse.conf) to have 'allow_others'\n"
             msg += "     and permission of '422'\n"
             raise CloudletFSError(msg)
     # pylint: enable=E1103
@@ -164,7 +173,7 @@ class CloudletFS(threading.Thread):
             self.fuse_write("terminate")
             self._pipe.close()
             self._pipe = None
-            #self.proc.terminate()
+            # self.proc.terminate()
 
 
 class StreamMonitor(threading.Thread):
@@ -176,8 +185,8 @@ class StreamMonitor(threading.Thread):
         self.monitor_file_list = list()
         self.stream_dict = dict()
         self._running = False
-        self.stop = False   # use boolean instead of Event to minimize 
-                            # Condition.wait() overhead in green thread 
+        self.stop = False   # use boolean instead of Event to minimize
+                            # Condition.wait() overhead in green thread
                             # of Openstack
         self.modified_chunk_dict = dict()
         self.modified_disk_queue = modified_disk_queue
@@ -191,7 +200,7 @@ class StreamMonitor(threading.Thread):
         # through fcntl()
         LOG.info("start monitoring at %s" % path)
         fd = os.open(path, os.O_RDONLY | os.O_NONBLOCK)
-        self.stream_dict[fd] = {'name':name, 'buf':'', 'path':path}
+        self.stream_dict[fd] = {'name': name, 'buf': '', 'path': path}
         self.monitor_file_list.append(fd)
 
     def del_path(self, name):
@@ -232,20 +241,17 @@ class StreamMonitor(threading.Thread):
         LOG.info("close Stream monitoring thread")
 
     def _handle(self, fd):
-        filepath = self.stream_dict[fd]['path']
         try:
             buf = os.read(fd, 1024*1024)
         except OSError as e:
             if e.errno == errno.EAGAIN or e.errno == errno.EWOULDBLOCK:
-                #print("error in reading errno.EAGAIN %s" % filepath)
                 time.sleep(0.1)
             else:
-                #print("error in reading %s" % filepath)
                 pass
             return
 
         # We got some output
-        lines = (self.stream_dict[fd]['buf']+ buf).split('\n')
+        lines = (self.stream_dict[fd]['buf'] + buf).split('\n')
         # Save partial last line, if any
         self.stream_dict[fd]['buf'] = lines.pop()
         for line in lines:
@@ -259,7 +265,7 @@ class StreamMonitor(threading.Thread):
             else:
                 raise IOError("Error, invalid stream")
                 break
-        # Yield to other thread (Eventlet) after reading data from file. 
+        # Yield to other thread (Eventlet) after reading data from file.
         time.sleep(0.1)
 
     def _handle_chunks_modification(self, line):
@@ -282,7 +288,6 @@ class StreamMonitor(threading.Thread):
             self.modified_chunk_dict[chunk] = ctime
             if self.modified_disk_queue is not None:
                 self.modified_disk_queue.put((chunk, ctime))
-            #LOG.debug("%s: %f, %d" % ("modification", ctime, chunk))
         except ValueError as e:
             LOG.debug("warning failed to handle modified chunks: %s" % str(e))
 
@@ -307,13 +312,13 @@ class StreamMonitor(threading.Thread):
 
 
 class FileMonitor(threading.Thread):
-    QEMU_LOG    = "QEMU_LOG"
+    QEMU_LOG = "QEMU_LOG"
 
     def __init__(self, path, name):
         self.stream_dict = dict()
         self._running = False
-        self.stop = False   # use boolean instead of thread to minimize 
-                            # Condition.wait() overhead in green thread 
+        self.stop = False   # use boolean instead of thread to minimize
+                            # Condition.wait() overhead in green thread
                             # of Openstack
         self.fd = os.open(path, os.O_RDONLY | os.O_NONBLOCK)
         self.buf = ''
@@ -345,20 +350,22 @@ class FileMonitor(threading.Thread):
             #sys.stdout.write("discard:(%s, %s)\n" % (event_time, data))
             pass
         else:
-            sys.stdout.write("invalid log: (%s)(%s)(%s)\n" % (event_time, header, data))
+            sys.stdout.write(
+                "invalid log: (%s)(%s)(%s)\n" %
+                (event_time, header, data))
 
     def terminate(self):
         self.stop = True
 
 
 class FuseFeedingProc(multiprocessing.Process):
+
     def __init__(self, fuse, input_pipename, END_OF_PIPE, **kwargs):
         self.fuse = fuse
         self.input_pipename = input_pipename
         self.END_OF_PIPE = END_OF_PIPE
         self.time_queue = None
         self.stop = threading.Event()
-        #threading.Thread.__init__(self, target=self.feeding_thread)
         multiprocessing.Process.__init__(self, target=self.feeding_thread)
 
     def feeding_thread(self):
@@ -367,7 +374,6 @@ class FuseFeedingProc(multiprocessing.Process):
         while(not self.stop.wait(0.00001)):
             self._running = True
             try:
-                #chunks = self.input_pipe.recv()
                 chunks_str = self.input_pipe.readline().strip()
                 if chunks_str == self.END_OF_PIPE:
                     break
@@ -376,9 +382,10 @@ class FuseFeedingProc(multiprocessing.Process):
             self.fuse.fuse_write(chunks_str)
 
         end_time = time.time()
-        if self.time_queue != None: 
-            self.time_queue.put({'start_time':start_time, 'end_time':end_time})
-        LOG.info("[FUSE] : (%s)-(%s)=(%s)\n" % \
+        if self.time_queue is not None:
+            self.time_queue.put(
+                {'start_time': start_time, 'end_time': end_time})
+        LOG.info("[FUSE] : (%s)-(%s)=(%s)\n" %
                 (start_time, end_time, (end_time-start_time)))
         self.fuse.fuse_write("END_OF_TRANSMISSION")
 
