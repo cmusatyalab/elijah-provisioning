679f717fbde9c7657bf1cb5154a1c20b9ce741aa
Blake Rouse <blake.rouse@canonical.com>
2017-11-15 14:51:25 -0500

    Add ability to set the port and set the default to 80 for the gateway.

--- elijah/gateway/__init__.py
+++ elijah/gateway/__init__.py
@@ -219,7 +219,7 @@ def index():
         return "Found"
 
 
-def run_server(config_file=None, overlays_path=None, debug=False):
+def run_server(config_file=None, overlays_path=None, debug=False, port=None):
     """Run the flask server."""
     if not config_file:
         config_file = os.path.join('/etc', 'cloudlet', 'config.yaml')
@@ -238,7 +238,7 @@ def run_server(config_file=None, overlays_path=None, debug=False):
     app.config['CLOUDLET_CONFIG'] = config
     app.config['CLOUDLET_STATE'] = {}
     app.config['OVERLAYS_PATH'] = overlays_path
-    app.run(debug=debug)
+    app.run(debug=debug, port=port)
 
 
 if __name__ == '__main__':
