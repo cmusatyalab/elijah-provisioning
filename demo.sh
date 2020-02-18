10ff3e063f63495e5b2004376d926db8049d6716
Thomas Eiszler <teiszler@cs.cmu.edu>
2020-02-18 09:16:11 -0500

    Capture time from launch to handoff.

--- demo.sh
+++ demo.sh
@@ -78,11 +78,15 @@ if [[ "$skip_bw" -eq 0 ]]; then
 fi
 
 waitforresponse "Launch VM on $src"
+launch_time="date +%s"
 title="horizon-demo"
 title="$title""$RANDOM"
 echo "+++Launching VM ($title) on $src..."
 nephele -r "$src" run "$snapshot" "$title" "$nephele_run_flags"
 
 waitforresponse "Handoff $title to $dest"
+handoff_time="date +%s"
+duration=handoff_time - launch_time
+echo "Time between launch and handoff (seconds): $duration"
 echo "+++Performing handoff of $title to $dest..."
 nephele -r "$src" handoff "$title" "$dest"
